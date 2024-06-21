ed03df977f6629e1f4b52b6f8e3321205b6017eb
Md Maruf Bin Salim Bhuiyain <mdmarufbinsalim@gmail.com>
2024-06-22 03:09:39 +0600

    u

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,12 +4,15 @@ import { Link } from "react-router-dom";
 export default function Nav() {
     return (
         <div className="flex justify-between items-center bg-gray-800 gap-2 md:px-24 w-full">
-            <div className="flex items-center p-4  gap-2">
-                <ChefHat className="w-12 h-12 text-[#f59e0b]" />
-                <p>
-                    YumChef
-                </p>
-            </div>
+            <Link to="/">
+                <div className="flex items-center p-4  gap-2">
+                    <ChefHat className="w-12 h-12 text-[#f59e0b]" />
+                    <p>
+                        YumChef
+                    </p>
+                </div>
+            </Link>
+
 
             <Link to="/add" className="flex items-center gap-2 p-4  text-[#f59e0b] text-center">
                 Add Recipe
