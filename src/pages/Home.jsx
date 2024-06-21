e322b7f9dfc744d94c9af17618cef65f6017f9af
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2024-06-22 03:07:36 +0600

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,16 +17,19 @@ function Home() {
             <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-8 w-full p-4 md:p-8 md:px-24 overflow-auto">
                 {recipes.map((recipe) => {
                     return (
-                        <div key={recipe.id} className="bg-gray-800 rounded-lg shadow-md flex flex-col gap-4 p-4 hover:bg-gray-700">
+                        <div key={recipe.id} className="bg-gray-800 rounded-lg shadow-md flex flex-col gap-4 p-4">
                             <img src={recipe.imageUrl} alt="image" className="w-full h-48 object-cover" />
                             <h2 className="text-[#f59e0b]">{recipe.title}</h2>
                             <p>{recipe.description}</p>
 
-                            <div className="flex md:justify-end gap-4 py-4 md:p-8">
-
-                                <Link to={`/edit/${recipe.id}`} className="flex gap-2 w-max">Edit <Edit2Icon className="w-6 h-6" />
+                            <div className="flex justify-end gap-4">
+                                <Link to={`/edit/${recipe.id}`} className="flex items-center bg-yellow-600 hover:bg-yellow-700 text-xl text-white px-4 py-2 rounded">
+                                    Edit
+                                    <Edit2Icon className="w-5 h-5 ml-2" />
                                 </Link>
-                                <Link to={`/recipe/${recipe.id}`} className="flex gap-2 w-max">View Recipe <EyeIcon className="w-6 h-6" />
+                                <Link to={`/recipe/${recipe.id}`} className="flex items-center bg-green-500 hover:bg-green-600 text-xl text-white px-4 py-2 rounded">
+                                    View Recipe
+                                    <EyeIcon className="w-6 h-6 ml-2" />
                                 </Link>
                             </div>
                         </div>
