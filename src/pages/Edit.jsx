51faf5379df15ce9e1b8117205293395badfd23d
Md Maruf Bin Salim Bhuiyain <mdmarufbinsalim@gmail.com>
2024-06-22 02:08:15 +0600

    tested setSelectedRecipieId function

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,7 +1,27 @@
- function Edit() {
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useGlobalContext } from "../contexts/RecipeContext";
+
+function Edit() {
+
+    const { id } = useParams();
+    const { selectedRecipe, setSelectedRecipeId } = useGlobalContext();
+    useEffect(() => {
+        setSelectedRecipeId(id);
+    }, [id]);
+
+
     return (
         <div>
             <h1>Edit Page</h1>
+            {selectedRecipe && (
+                <div>
+                    <h2>{selectedRecipe.title}</h2>
+                    <p>{selectedRecipe.description}</p>
+                    <img src={selectedRecipe.imageUrl} alt="image" />
+                    <p>{selectedRecipe.steps}</p>
+                </div>
+            )}
         </div>
     );
 }
